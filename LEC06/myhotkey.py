62932df6a2705b11b38c4c3263eb3b12845faebe
kimi5231 <djwjod@naver.com>
2024-04-02 00:18:01 +0900

    LEC06 보충

--- LEC06/myhotkey.py
+++ LEC06/myhotkey.py
@@ -1,9 +1,10 @@
 import keyboard
 
-def write_1000numbers():
+
+def write_10numbers():
     keyboard.write(''.join(str(n) for n in range(10)))
 
 
-keyboard.add_hotkey('shift+windows+w', write_1000numbers)
+keyboard.add_hotkey('shift+windows+w', write_10numbers)
 keyboard.wait('esc') # 무한루프 esc 종료
 keyboard.remove_all_hotkeys()
--- LEC06/string_lab.py
+++ LEC06/string_lab.py
@@ -19,7 +19,11 @@ END.
 print_lines()
 
 # Multiline Comments.
-""""""
+"""This is a test Python program.
+Written by Al Sweigart al@inventwithpython.com
+
+This program was designed for Python 3, not Python 2.
+"""
 
 # 리스트 슬라이스와 동일.
 message = 'hello, world'
@@ -54,24 +58,55 @@ spam
 # 모든 알파벳을 소문자로 변경.
 spam = spam.lower()
 spam
+# 모든 알파벳이 소문자인가?
+spam.islower()
+# 모든 알파벳이 대문자인가?
+spam.isupper()
+
+'HELLO'.isupper()
+'abc12345'.islower()
+'12345'.islower()
+'12345'.isupper()
+
+# 뒤에 적힌 문자열로 시작하는가?
+'Hello world!'.startswith('Hello')
+'abc123'.startswith('abcdef')
+# 뒤에 적힌 문자열로 끝나는가?
+'Hello world!'.endswith('world!')
+'abc123'.endswith('12')
 
 # 앞에 있는 구별 문자열을 이용해 리스트에 있는 문자열들을 합친다.
 ':'.join(['cat', 'rat', 'bat'])
 
 ''.join(str(i) for i in range(20))
 
-# 기준에 따라 문자열을 분리.
+# 기준에 따라 문자열을 분리. (모든 공백 space, \n \t \v \f \r)
 'my name is sooyoung'.split()
 'my name is sooyoung\ni love you'.split()
 'my name is sooyoung\ti love you'.split()
+'my name is sooyoung i love you'.split('o')
+# 줄 단위로 분리.
+spam = '''Dear Alice,
+How have you been? I am fine.
+There is a container in the fridge
+that is labeled "Milk Experiment".
+
+Please do not drink it.
+Sincerely,
+Bob'''
+spam.split('\n')
 
 # 문자열을 separator 문자열을 기준 삼아 세개의 문자열로 분리.
+# before, separator, after
+'Hello, world!'.partition('w')
+'Hello, world!'.partition('world')
+'Hello, world!'.partition('XYZ')
 
 # 문자열 정렬.
 'hello'.rjust(10)
 'hello'.ljust(10)
 'hello'.center(10)
-'hello'.center(10, '=')
+'hello'.center(11, '=')
 
 def print_picnic(food, lwidth, rwidth):
     print('PICNIC FOOD'.center(lwidth+rwidth, '='))
@@ -90,16 +125,6 @@ spam.lstrip()
 # 오른쪽에 있는 문자만 제거.
 spam.rstrip()
 
-spam = 'Hello World!'
-# 문자열의 문자가 모두 소문자인가?
-spam.islower()
-# 문자열의 문자가 모두 대문자인가?
-spam.isupper()
-'HELLO'.isupper()
-'abc12345'.islower()
-'12345'.islower()
-'12345'.isupper()
-
 # 아스키코드에 따라 문자를 숫자로 변경.
 ord('A')
 # 아스키코드에 따라 숫자를 문자로 변경.
@@ -107,5 +132,8 @@ chr(65)
 
 # pyperclip.
 import pyperclip
+# 문자열을 클립보드에 저장.
 pyperclip.copy('hello, world')
 pyperclip.copy(''.join(str(n) for n in range(1000)))
+# 클립보드에 있는 내용을 가져옴.
+pyperclip.paste()
